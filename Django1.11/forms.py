bf5ed81d3981aeebfd64442c45052a3377835763
Frank Sustaita <fsequence@gmail.com>
2019-05-31 16:46:32 -0700

    Update forms.py

--- Django1.11/forms.py
+++ Django1.11/forms.py
@@ -1,1 +1,32 @@
+import unicodedata
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+# looked at the source code and UserCreationForm and UserChangeForm
+# need/use the class UsernameField, so here it is, included it
+class UsernameField(forms.CharField):
+    def to_python(self, value):
+        return unicodedata.normalize('NFKC', super().to_python(value))
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'password1', 'password2')
+        
+        # deleted 'username' and put 'email' instead, that should be correct, :]
+        field_classes = {'email': UsernameField}
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name',)
+       
+        # deleted 'username' and put 'email' instead, that should be correct, :]
+        field_classes = {'email': UsernameField}
 
