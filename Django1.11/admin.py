5b9b339040f990fb23ac586c207c927ea46b0efe
Frank Sustaita <fsequence@gmail.com>
2019-05-31 16:52:00 -0700

    Update admin.py

--- Django1.11/admin.py
+++ Django1.11/admin.py
@@ -1,1 +1,41 @@
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User(the default user model)
+    # located at django.contrib.auth.User
+    list_display = ('email', 'first_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    fieldsets = (
+        (_('User'), {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                            'groups', 'user_permissions')}),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
